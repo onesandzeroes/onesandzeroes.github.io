e7ddc7c17396ea38cc2e4f5aadced13795a813da
onesandzeroes <onesandzeroes@github.com>
2014-10-21 14:48:19 +1100

    Fancy transitions

--- beta.js
+++ beta.js
@@ -31,25 +31,30 @@ function update_graph() {
         .domain([0, d3.max(dataset, function(d) {return d[1];})])
         .range([height - (2 * padding), 2 * padding]);
     var y_axis = d3.svg.axis()
-        .scale(y_scale)
         .ticks(10)
-        .orient("left");
+        .orient("left")
+        .scale(y_scale);
 
     var line_creator = d3.svg.line()
         .x(function(d) { return x_scale(d[0]); })
         .y(function(d) { return y_scale(d[1]); });
 
     svg.select("path")
-        .attr("d", line_creator(dataset))
+        .attr("class", "data-line")
         .attr("stroke", "red")
-        .attr("fill", "white");
+        .attr("fill", "white")
+        .transition()
+        .duration(800)
+        .ease("bounce   ")
+        .attr("d", line_creator(dataset));
 
-    svg.select(".x-axis").remove();
+    svg.select("#x-axis").remove();
 
-    svg.select(".y-axis").remove();
+    svg.select("#y-axis").remove();
 
     svg.append("g")
-        .attr("class", "x-axis")
+        .attr("class", "axis")
+        .attr("id", "x-axis")
         .attr("transform", "translate(0, " + (height - 2 * padding) + ")")
         .call(x_axis);
     svg.append("text")
@@ -58,7 +63,8 @@ function update_graph() {
         .style("text-anchor", "middle")
         .text("y");
     svg.append("g")
-        .attr("class", "y-axis")
+        .attr("class", "axis")
+        .attr("id", "y-axis")
         .attr("transform", "translate(" + (2 * padding) + ", 0)")
         .call(y_axis);
     svg.append("text")
